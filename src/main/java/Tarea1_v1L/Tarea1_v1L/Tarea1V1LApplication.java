73a879c9054c37bd72fb1d8dd22a26f629357e06
TheRavenStark <113565611+TheRavenStark@users.noreply.github.com>
2024-02-04 16:50:52 -0600

    Initial commit

---
+++ src/main/java/Tarea1_v1L/Tarea1_v1L/Tarea1V1LApplication.java
@@ -0,0 +1,13 @@
+package Tarea1_v1L.Tarea1_v1L;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea1V1LApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea1V1LApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tarea1_v1L/Tarea1_v1L/Tarea1V1LApplicationTests.java
@@ -0,0 +1,13 @@
+package Tarea1_v1L.Tarea1_v1L;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea1V1LApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
